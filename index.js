98f695e07989f8e3e49f170fc8df2867a4045276
mankowsj <22179350+mankowsj@users.noreply.github.com>
2019-07-25 09:40:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,62 +3,73 @@
  * @date 24/07/2019
  */
 
-const hookName = 'require-extensions';
-
-function createPublicPathFunction(pathType, pathParam) {
-    switch (pathType) {
-        case 'function':
-            return pathParam.toString();
-        default:
-            return '';
-    }
-}
-
-function createPublicPathCall(pathType, pathParam) {
-    switch (pathType) {
-        case 'function':
-            return `${pathParam.name}(href)`;
-        default:
-            return pathParam;
-    }
-}
-
-function applyCustomPublicPath(path, source) {
-    const pathType = typeof path;
-    const code = [];
-    code.push(source);
-    code.push(createPublicPathFunction(pathType, path));
-    code.push('__webpack_require__.p = ' + createPublicPathCall(pathType, path) + ' || __webpack_require__.p;');
-    return code.join('\n');
-}
-
 class WebpackCustomPublicPathPlugin {
-    constructor(options) {
-        this.options = options || {};
-        this.pluginName = 'WebpackCustomPublicPathPlugin';
+  constructor(options) {
+    this.options = options || {};
+
+    this.hookName = 'require-extensions';
+    this.paramName = 'href';
+    this.pluginName = 'WebpackCustomCssPublicPathPlugin';
+  }
+
+  getPublicPathReplacement(path, pathType) {
+    switch (pathType) {
+      case 'function':
+        return `${path.name}(${this.paramName})`;
+      default:
+        return `${path} + ${this.paramName}`;
+    }
+  }
+
+  getPublicPathFunction(path, pathType) {
+    switch (pathType) {
+      case 'function':
+        return path.toString();
+      default:
+        return '';
+    }
+  }
+
+  applyCustomPublicPath(path, source) {
+    const result = [];
+    const pathType = typeof path;
+    const regexp = /__webpack_require__\.p(\s|)\+(\s|)href/;
+
+    if (regexp.test(source)) {
+      result.push(this.getPublicPathFunction(path, pathType));
+      result.push(
+        source.replace(regexp, this.getPublicPathReplacement(path, pathType))
+      );
+    } else {
+      result.push(source);
+      console.error(this.pluginName, ' failed to change publicPath for css');
     }
 
-    apply(compiler) {
-        const publicPath = this.options.publicPath;
-        if (!publicPath) {
-            console.error('RuntimePublicPath: no output.runtimePublicPath is specified.');
-            return;
-        }
+    return result.join('\n');
+  }
 
-        if (compiler.hooks && compiler.hooks.thisCompilation) {
-            compiler.hooks.thisCompilation.tap(this.pluginName, function (compilation) {
-                compilation.mainTemplate.plugin(hookName, function (source, chunk, hash) {
-                    return applyCustomPublicPath(publicPath, source)
-                });
-            });
-        } else {
-            compiler.plugin('this-compilation', function (compilation) {
-                compilation.mainTemplate.plugin(hookName, function (source, chunk, hash) {
-                    return applyCustomPublicPath(publicPath, source)
-                });
-            });
-        }
+  apply(compiler) {
+    const self = this;
+    const publicPath = this.options.cssPublicPath;
+    if (!publicPath) {
+      console.error('RuntimePublicPath: no output.runtimePublicPath is specified.');
+      return;
     }
+
+    if (compiler.hooks && compiler.hooks.thisCompilation) {
+      compiler.hooks.thisCompilation.tap(self.pluginName, function (compilation) {
+        compilation.mainTemplate.plugin(self.hookName, function (source, chunk, hash) {
+          return self.applyCustomPublicPath(publicPath, source);
+        });
+      });
+    } else {
+      compiler.plugin('this-compilation', function (compilation) {
+        compilation.mainTemplate.plugin(self.hookName, function (source, chunk, hash) {
+          return self.applyCustomPublicPath(publicPath, source);
+        });
+      });
+    }
+  }
 }
 
 module.exports = WebpackCustomPublicPathPlugin;
